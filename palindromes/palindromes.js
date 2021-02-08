cd2b544e6dc611c6559bc1ed9fa825ce3e4fb420
PFunklight666 <63506496+PFunklight666@users.noreply.github.com>
2021-02-08 06:41:25 -0500

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  str = str.toLowerCase().replace(/[\W_]/g, "");
+  for (var i = 0, len = str.length - 1; i < len / 2; i++) {
+    if (str[i] !== str[len - i]) {
+      return false;
+    }
+return true;
 }
 
 module.exports = palindromes
